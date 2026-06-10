656f2857773144f1ad7b29b70c602eb01aa8f2ee
Saiprasath-2004 <saiprasath2004@gmail.com>
2026-06-10 17:34:40 +0530

    refactored the Folder structure to segregrate each days tasks

---
+++ Day-3 tasks/01_Number_classifier.py
@@ -0,0 +1,14 @@
+number = int(input("Enter the Number: "))
+
+if number > 0 :
+    print(" The given number is Positive")
+elif number< 0:
+    print(" The given number is Negative")
+else:
+    print(" The given number is Zero")
+
+
+if number % 2 == 0:
+    print(" The given Number is Even")
+else:
+    print(" The given Number is Odd")
